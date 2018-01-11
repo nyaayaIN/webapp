8ef3f7afdeeab40e450e2c7bd168e9dcf16b9fcf
Saurabh Ariyan <saurabhkumar380@gmail.com>
2018-01-11 14:21:24 +0530

    fixed jest snapshot and fetch

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,6 +8,7 @@
  */
 
 import React from 'react';
+import fetch from 'isomorphic-fetch';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.css';
 import Link from '../Link';
