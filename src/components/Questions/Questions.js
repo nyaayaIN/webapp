b9fd4d891e1cbe0352b3c5177e2754aa1746b848
Archana Kumar <archanks@gmail.com>
2018-02-01 20:00:30 +0530

    Follow Us and Featured Questions

---
+++ src/components/Questions/Questions.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import PropTypes from 'prop-types';
+import s from './Questions.css';
+import Link from '../Link';
+
+class Questions extends React.Component {
+  static propTypes = {
+    collection: PropTypes.arrayOf(
+      PropTypes.shape({
+        question: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+  };
+
+  render() {
+    return (
+      <ul className={s.questions}>
+        {this.props.collection.map(item => (
+          <li className={s.question}>
+            <Link to={item.url} className={s.questionLink}>
+              <p className={s.questionText}>{item.question}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default withStyles(s)(Questions);
