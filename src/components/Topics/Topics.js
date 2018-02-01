153663f620745a515c715c1e872f9a6dc8273e91
Archana Kumar <archanks@gmail.com>
2018-02-01 16:09:08 +0530

    Topics and Hero

---
+++ src/components/Topics/Topics.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import PropTypes from 'prop-types';
+import s from './Topics.css';
+import Link from '../Link';
+
+class Topics extends React.Component {
+  static propTypes = {
+    collection: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+  };
+
+  render() {
+    return (
+      <div className={s.topics}>
+        {this.props.collection.map(topic => (
+          <Link className={s.topic} to={topic.url}>
+            <div className={s.tile}>
+              <div className={s.title}>{topic.name}</div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default withStyles(s)(Topics);
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,123 +1,49 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
+import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
-import heroContent from './hero.md';
-import content from './home.md';
-import Link from '../../components/Link';
-
-const API = '/data/featured';
+import Topics from '../../components/Topics';
+import Hero from '../../components/Hero';
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      featured: [],
-    };
-  }
-
-  componentDidMount() {
-    fetch(API)
-      .then(response => {
-        if (!response || !response.ok) {
-          throw new Error(response.statusText || 'No Response');
-        }
-        return response;
-      })
-      .then(response => response.json())
-      .then(data => this.setState({ featured: data }))
-      .catch(error => {
-        // eslint-disable no-console
-        console.error(error.message);
-      });
-  }
+  static propTypes = {
+    hero: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+    }).isRequired,
+    headings: PropTypes.shape({
+      featured_topics: PropTypes.string.isRequired,
+      what_we_do: PropTypes.string.isRequired,
+      follow_us: PropTypes.string.isRequired,
+      word_of_the_day: PropTypes.string.isRequired,
+      featured_questions: PropTypes.string.isRequired,
+    }).isRequired,
+    featured: PropTypes.shape({
+      topics: PropTypes.arrayOf(
+        PropTypes.shape({
+          name: PropTypes.string.isRequired,
+          url: PropTypes.string.isRequired,
+        }),
+      ).isRequired,
+      questions: PropTypes.arrayOf(
+        PropTypes.shape({
+          question: PropTypes.string.isRequired,
+          url: PropTypes.string.isRequired,
+        }),
+      ).isRequired,
+    }).isRequired,
+  };
 
   render() {
-    let featuredTopics;
-    if (this.state.featured.length) {
-      featuredTopics = (
-        <div className={s.featured}>
-          <div className={s.container}>
-            <h1 className={s.title}>{content.featured}</h1>
-            <div className={s.featuredTopics}>
-              {this.state.featured.map(topic => (
-                <div className={s.featuredTopic}>
-                  <Link to={topic.url}>
-                    <div className={s.featuredTopicTitle}>{topic.name}</div>
-                  </Link>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      );
-    }
     return (
-      <div>
-        <div className={s.hero}>
-          <div className={s.container}>
-            <h1 className={s.heroTitle}>{heroContent.lead}</h1>
-            <div
-              className={s.heroContent}
-              // eslint-disable-next-line react/no-danger
-              dangerouslySetInnerHTML={{ __html: heroContent.html }}
-            />
-            <form
-              action="https://nyaaya.createsend.com/t/d/s/fjrjil/"
-              method="post"
-              id="subForm"
-              className={s.subscriberForm}
-            >
-              <input
-                className={s.subscribeInput}
-                id="fieldName"
-                name="cm-name"
-                type="text"
-                placeholder={content.name}
-              />
-              <input
-                className={s.subscribeInput}
-                id="fieldEmail"
-                name="cm-fjrjil-fjrjil"
-                type="email"
-                placeholder={content.email}
-                required
-              />
-              <button className={s.subscribeSubmit} type="submit">
-                {content.subscribe}
-              </button>
-            </form>
-          </div>
-        </div>
-        {featuredTopics}
-        <div className={s.usp}>
-          <div className={s.container}>
-            <h1 className={s.title}>{content.usp}</h1>
-            <div
-              className={s.uspContent}
-              // eslint-disable-next-line react/no-danger
-              dangerouslySetInnerHTML={{ __html: content.html }}
-            />
-          </div>
-        </div>
-        <div className={s.follow}>
-          <div className={s.container}>
-            <h1 className={s.title}>{content.follow}</h1>
-            <div className={s.socialMedia}>
-              <div className={s.social}>facebook</div>
-              <div className={s.social}>twitter</div>
-              <div className={s.social}>linkedin</div>
-              <div className={s.social}>Medium</div>
-            </div>
-          </div>
+      <div className={s.root}>
+        <Hero content={this.props.hero} />
+
+        <div className={s.container}>
+          <div className={s.heading}>{this.props.headings.featured_topics}</div>
+          <Topics collection={this.props.featured.topics} />
         </div>
       </div>
     );
